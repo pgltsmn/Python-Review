367e45fc1d147fb77c7af423aeab8508f2143ba1
pgltsmn <93178841+pgltsmn@users.noreply.github.com>
2022-07-24 18:24:46 +0300

    privet
    i haven't finished it yet but i will

---
+++ pythonReview.py
@@ -0,0 +1,24 @@
+def create_youtube_video(title, description):
+	youtubevideo = {
+		"title": title,
+		"description": description,
+		"likes": 0,
+		"dislikes": 0,
+		"comments": comments = {} #empty dictionary to add to
+	}
+	return youtubevideo
+
+def like(create_youtube_video):
+	if "likes" in youtubevideo:
+		youtubevideo[likes] = youtubevideo[likes] + 1
+
+def dislike(create_youtube_video):
+	if "dislikes" in youtubevideo:
+		youtubevideo[dislikes] = youtubevideo[dislikes] + 1
+
+def add_comment(youtubevideo, username, comment_text):
+
+
+
+
+
