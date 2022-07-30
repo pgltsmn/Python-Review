22bb39a6977b088ee88571181434dd65480c97db
pgltsmn <93178841+pgltsmn@users.noreply.github.com>
2022-07-30 14:41:47 +0300

    did it fully

--- pythonReview.py
+++ pythonReview.py
@@ -4,19 +4,26 @@ def create_youtube_video(title, description):
 		"description": description,
 		"likes": 0,
 		"dislikes": 0,
-		"comments": comments = {} #empty dictionary to add to
+		"comments": {} #an empty dictionary to add to
 	}
 	return youtubevideo
 
-def like(create_youtube_video):
+def like(youtubevideo{}):
 	if "likes" in youtubevideo:
 		youtubevideo[likes] = youtubevideo[likes] + 1
+		return youtubevideo
 
-def dislike(create_youtube_video):
+def dislike(youtubevideo{}):
 	if "dislikes" in youtubevideo:
 		youtubevideo[dislikes] = youtubevideo[dislikes] + 1
+		return youtubevideo
 
-def add_comment(youtubevideo, username, comment_text):
+def add_comment(youtubevideo{}, username, comment_text):
+	youtubevideo[comments['username']] = comment_text
+	return youtubevideo
+
+create_youtube_video('my life etc', 'not really fun dont expect smth')
+
 
 
 
